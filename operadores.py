85b47814e6d86ae9e07909d7b86792d3a208029e
Lennna <danna.coello@tecnologicoliceocristiano.edu.ec>
2024-09-14 10:54:06 -0500

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,63 @@
+a = input("Introduce valor a: ")
+b = input("Introduce valor b: ")
+c = input("Introduce valor c: ")
+d = input("Introduce valor d: ")
+    
+    
+
+if a == b:
+    print('A y B son iguales')
+else: 
+    print('A y B no son iguales')
+    
+if b == c:
+    print('B y C son iguales')
+else: 
+    print('B y C no son iguales')
+    
+if c == d:
+    print('C y D son iguales')
+else: 
+    print('C y D no son iguales')
+    
+if c == a:
+    print('C y A son iguales')
+else: 
+    print('C y A no son iguales')
+    
+    
+if b == d:
+    print('B y D son iguales')
+else:
+    print('B y D no son iguales')
+
+print("Todos los valores del primer ejercicio han sido ejecutados")
+
+
+if a < b:
+    print('A es mayor que B')
+else: 
+    print('A es menor que B')
+    
+if b < c:
+    print('B es mayor que C')
+else: 
+    print('B es menor C')
+    
+if c < d:
+    print('C es mayor que D')
+else: 
+    print('C es menor D')
+    
+if c < a:
+    print('C es mayor que A')
+else: 
+    print('C es menor A')
+    
+    
+if b < d:
+    print('B es mayor que D')
+else:
+    print('B es menor D')
+    
+print("Todos los valores del segundo ejercicio han sido ejecutados")
